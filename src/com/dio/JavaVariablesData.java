70155fce2b81fd22b4ac231e9fdfe95d8ee3fd28
micribeiro <micael.jesse@gmail.com>
2022-01-02 08:42:07 -0300

    Primeiro commit de código Java

---
+++ src/com/dio/JavaVariablesData.java
@@ -0,0 +1,18 @@
+package com.dio;
+
+public class JavaVariablesData {
+
+	public static void main(String[] args) {
+		int minhaIdade = 5;
+		int idadeIrmao = 6;
+		final int IDADE_FIXA = 18;
+		
+		int soma = minhaIdade + idadeIrmao + IDADE_FIXA;
+		
+		System.out.println("A soma dos três números do código é: " + soma);
+		System.out.println("O objetivo desse programa é declarar e trabalhar "
+				+ "com variáveis pondo em prática boas práticas em Java.");
+
+	}
+
+}
